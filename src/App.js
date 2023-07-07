f46cd1a471d99319dfad6a8748a8267864a673cb
Ravi Yadav <raviyadav29032003@gmail.com>
2023-07-07 10:27:15 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ParaText from './Component/ParaText';
+
+function App() {
+  return (
+    <div className="App">
+    <ParaText/>
+    </div>
+  );
+}
+
+export default App;
